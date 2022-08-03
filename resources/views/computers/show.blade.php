30d989d34ca8a99bb3923cf9a43d82c2d6792bf8
ChakkerAmine <chakkeramine18@gmail.com>
2022-08-03 10:17:47 +0100

    Adding show view to display local data

---
+++ resources/views/computers/show.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>showing Computer</title>
+</head>
+<body>
+    <h1>Computer</h1>
+    <hr>
+    <div class="data">
+        <h3>{{$computer['name']}} is from <strong>{{$computer['origin']}}</strong> </h3>
+    </div>
+</body>
+</html>
